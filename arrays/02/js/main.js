29639802101436d56d62ec0bde214bf2398bd81d
kluchkos <semenkluchko@gmail.com>
2021-03-03 18:08:35 +0200

    add first part of second task

---
+++ arrays/02/js/main.js
@@ -0,0 +1,21 @@
+;let check = [
+	{ name: "Milk", quantity: 3, price: 6.9, },
+  { name: "Pork", quantity: 2, price: 20, },
+  { name: "Beef", quantity: 5, price: 42, },
+  { name: "Potatoes", quantity: 6, price: 14, },
+  { name: "Cheese", quantity: 1, price: 36, },
+];
+
+
+// First Task
+const getMagazineCheck = function(list) {
+let result; 
+
+for (let key in list) {
+ result = console.log("Product: " + list[key].name + "; Quantity: " +  list[key].quantity + "; Price: " + list[key].price + "$");
+}
+
+return result;
+};
+
+getMagazineCheck(check);
