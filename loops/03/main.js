3378e58344316d68586c783481b845b02dbc49d1
kluchkos <semenkluchko@gmail.com>
2021-03-11 16:51:44 +0200

    fix

--- loops/03/main.js
+++ loops/03/main.js
@@ -5,9 +5,9 @@
 let result = 1;
 
 for (let i = number; i > 0; i-- ) {
-		if(number % i === 0) {
-    result = i;
-    console.log(result);
+  if(number % i === 0) {
+  result = i;
+  console.log(result);
     }
 }
 
